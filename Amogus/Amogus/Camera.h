31fc1d71d6fbc588e81da0f78ba52b92118526ce
Corbin Mottram <Mottramc@sky.com>
2022-01-17 22:13:48 +0000

    Added Commented out get/set function
    May have future utility, but can be removed if unnecessary

--- Amogus/Amogus/Camera.h
+++ Amogus/Amogus/Camera.h
@@ -1,8 +1,22 @@
 #pragma once
 class Camera
 {
+private:
+	//int m_viewportWidth;
+	//int m_viewportHeight;
+
+public:
 	int m_viewportWidth;
 	int m_viewportHeight;
+
 	//glm::mat4 view;
+
+	//void SetViewportWidth(int newWidth) { m_viewportWidth = newWidth; }
+	//void SetViewportHeight(int newHeight) { m_viewportWidth = newHeight; }
+	//void SetView(glm::vec3 position, glm::vec3 target, glm::vec3 vector) { view = glm::lookAt(position, target, vector);	}
+
+	//int GetViewportWidth() { return m_viewportWidth; }
+	//int GetViewportHeight() { return m_viewportHeight; }
+	//glm::mat4 GetView() { return view; }
 };
 
