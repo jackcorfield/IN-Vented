41e67269986e0f787b2324f457ba0683ce4baa99
Tom Silver <tomajsilver@gmail.com>
2022-01-18 11:09:20 +0000

    Renderer now uses camera
    Also cleaned up renderer constructor and camera class

--- Amogus/Amogus/Camera.h
+++ Amogus/Amogus/Camera.h
@@ -1,22 +1,18 @@
 #pragma once
+
 class Camera
 {
-private:
-	//int m_viewportWidth;
-	//int m_viewportHeight;
-
 public:
+	Camera() :
+		Camera(0, 0)
+	{}
+	Camera(const int viewportWidth, const int viewportHeight) :
+		m_viewportWidth(viewportWidth),
+		m_viewportHeight(viewportHeight)
+	{}
+
 	int m_viewportWidth;
 	int m_viewportHeight;
 
 	//glm::mat4 view;
-
-	//void SetViewportWidth(int newWidth) { m_viewportWidth = newWidth; }
-	//void SetViewportHeight(int newHeight) { m_viewportWidth = newHeight; }
-	//void SetView(glm::vec3 position, glm::vec3 target, glm::vec3 vector) { view = glm::lookAt(position, target, vector);	}
-
-	//int GetViewportWidth() { return m_viewportWidth; }
-	//int GetViewportHeight() { return m_viewportHeight; }
-	//glm::mat4 GetView() { return view; }
 };
-
