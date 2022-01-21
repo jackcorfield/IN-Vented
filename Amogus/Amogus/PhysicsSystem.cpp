cc2625e3609fceef47e187ea76e85f753ce0e11d
Jack Corfield <jackcorfield1999@gmail.com>
2022-01-21 15:18:10 +0000

    Revert "Revert "Merge branch 'develop' into feature/Editor-UI""
    This reverts commit 61574062ad94ade1ebb1476b46c60ba6245a3d96.

---
+++ Amogus/Amogus/PhysicsSystem.cpp
@@ -0,0 +1,41 @@
+#include "PhysicsSystem.h"
+#include "source.h"
+
+extern Application* g_app;
+
+PhysicsSystem::PhysicsSystem()
+{
+
+
+}
+
+void PhysicsSystem::PhysicsUpdate(float deltaTime)
+{
+	Scene* activeScene = g_app->m_sceneManager->GetActiveScene();
+    std::vector<Physics*> physicsComponents = activeScene->m_entityManager->GetAllComponentsOfType<Physics>();
+
+    for (Physics* p : physicsComponents)
+    {
+        Entity physicsEntity = activeScene->m_entityManager->GetEntityFromComponent<Physics>(p);
+        Transform* transform = activeScene->m_entityManager->GetComponent<Transform>(physicsEntity);
+        MovementPhysics(p, transform, deltaTime);
+    }	
+}
+
+void PhysicsSystem::MovementPhysics(Physics* physics, Transform* transform, float deltaTime)
+{
+    physics->UpdateAcceleration();
+    physics->UpdateVelocity();
+    //REPLACE WITH DELTA TIME PLS
+    CalculateMovement(physics, transform, deltaTime);
+}
+
+void PhysicsSystem::CalculateMovement(Physics* physics, Transform* transform, const float deltaTime)
+{
+    // update world position of object by adding displacement to previously calculated position
+    physics->SetPosition(transform->m_position);
+    physics->SetPosition(physics->addScaledVector(physics->GetPosition(), physics->GetVelocity(), deltaTime));
+
+
+    transform->m_position = physics->GetPosition();
+}
