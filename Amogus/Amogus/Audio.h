c7975f00187fb6ed37f9913f7a8e6cc6af232ed3
Corbin Mottram <48811130+Kyuba42@users.noreply.github.com>
2022-01-28 14:48:24 +0000

    Audio improvements (#42)
    * Improved Audio for ECS
    
    * Update Audio.h
    
    * Update imgui.ini

--- Amogus/Amogus/Audio.h
+++ Amogus/Amogus/Audio.h
@@ -10,28 +10,10 @@ public:
 	Audio(const char* path, FMOD::System* system, FMOD::ChannelGroup* group);
 	~Audio();
 
-	bool AudioInit();
-
-	void AddAudio(const char* path);
-	void RemoveAudio();
-
-	void AddToGroup(FMOD::ChannelGroup* group);
-
-	void PauseAudio();
-	void UnpauseAudio();
-
-	void PlayAudio();
-	void Loop();
-
-	bool IsPlaying = false;
-
-	FMOD::Channel* m_channel;
-private:
-
-	FMOD::System*		m_system;
+	FMOD::Channel* m_channel =0;
+	FMOD::Sound* m_sound;
+	FMOD::System* m_system;
 	FMOD::ChannelGroup* m_group;
-	FMOD::Sound*			m_sound;
-	FMOD_RESULT		m_result;
-	void*						m_extradriverdata = 0;
+	
 };
 
