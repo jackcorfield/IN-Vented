cdf2b9b338c45cd75e9c434bbdfbae949f45972c
Hallett-K <h013486i@student.staffs.ac.uk>
2022-05-11 12:46:03 +0100

    HOTFIX: Transparency
    Images with alpha of 0 are now fully transparent

--- Amogus/YokaiFortune/Assets/Shaders/DefaultSpriteF.glsl
+++ Amogus/YokaiFortune/Assets/Shaders/DefaultSpriteF.glsl
@@ -13,5 +13,8 @@ uniform vec3 spriteColor;
 void main()
 {    
     color = vec4(spriteColor, 1.0) * texture(image, fInput.texCoords);
-    //color = vec4(0.5, 0.5, 0.5, 1.0);
+    if (color.a == 0.0)
+    {
+        discard;
+	}
 }
