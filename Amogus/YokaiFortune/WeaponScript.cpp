68ec801a585ab028aa37ff818b07d740f41c2ed5
Corbin Mottram <48811130+Kyuba42@users.noreply.github.com>
2022-05-12 10:36:26 +0100

    Level Up function added

--- Amogus/YokaiFortune/WeaponScript.cpp
+++ Amogus/YokaiFortune/WeaponScript.cpp
@@ -1,1 +1,42 @@
 #include "WeaponScript.h"
+
+
+WeaponScript::WeaponScript(EntityManager* entityManager, Entity parentEntityID, int level) :Script(entityManager, parentEntityID),
+m_manager(entityManager),
+m_player(parentEntityID),
+m_canLevel(true),
+m_currentLevel(level)
+{
+	//AddComponent<Sprite>(m_player, newTexture, newColour, newShader);
+	//AddComponent<BoxCollider>(m_player, (GetComponent<Sprite>()->GetTexture().m_width, GetComponent<Sprite>()->GetTexture().m_height));
+}
+
+void WeaponScript::OnLevelUp()
+{
+	if (!m_currentLevel < m_maxLevel)
+		return;
+
+	m_currentLevel++;
+
+	switch (m_levelingInfo[m_currentLevel].first)
+	{
+	case SPEED:
+		m_baseProjectileSpeed += m_levelingInfo[m_currentLevel].second;
+		break;
+	case	COOLDOWN:
+		m_baseProjectileCooldown += m_levelingInfo[m_currentLevel].second;
+		break;
+	case AREA:
+		m_baseProjectileArea += m_levelingInfo[m_currentLevel].second;
+		break;
+	case	DURATION:
+		m_baseProjectileDuration += m_levelingInfo[m_currentLevel].second;
+		break;
+	case	COUNT:
+		m_baseProjectileCount += m_levelingInfo[m_currentLevel].second;
+		break;
+	case DAMAGE:
+		m_baseDamageModifier += m_levelingInfo[m_currentLevel].second;
+		break;
+	}
+}
--- Amogus/YokaiFortune/WeaponScript.h
+++ Amogus/YokaiFortune/WeaponScript.h
@@ -6,33 +6,48 @@ class WeaponScript :
     public Script
 {
 public:
-    WeaponScript(EntityManager* entityManager, Entity parentEntityID, float speed);
+    WeaponScript(EntityManager* entityManager, Entity parentEntityID, int level);
     ~WeaponScript();
     void OnAttach();
     void OnUpdate(float dt);
     void OnRender(float dt);
     void OnUnattach();
 
+    void OnLevelUp();
+
 private:
+    enum modifier
+    {
+        SPEED = 0,
+        COOLDOWN,
+        AREA,
+        DURATION,
+        COUNT,
+        DAMAGE
+    };
+
+    Entity m_player;
+    EntityManager* m_manager;
 
     int m_maxLevel;
 
-    BoxCollider m_boxCollider;
-    Sprite  m_sprite;
+    bool m_canLevel;
+    int m_currentLevel;
+
     //Modifiers
 
-    float m_hitDelay;
-
-    float m_baseProjectileSpeed; //Speed of projectiles
-    float m_baseProjectileCooldown; //How often weapon attacks
-    float m_baseProjectileArea; //Size of weapon
-    float m_baseProjectileDuration; //How long the projectile stays on the screen
-
-    float m_baseProjectileCount; //How many projectiles
-    float m_projectileMax;
+    int m_baseProjectileSpeed; //Speed of projectiles
+    int m_baseProjectileCooldown; //How often weapon attacks
+    int m_baseProjectileArea; //Size of weapon
+    int m_baseProjectileDuration; //How long the projectile stays on the screen
+    int m_baseProjectileCount; //How many projectiles
+    int m_projectileMax;
     
-    float m_baseDamageModifier;
+    int m_baseDamageModifier;
 
+    float m_hitDelay;
     float m_critMultiplier;
+
+    std::vector<std::pair<modifier, int>> m_levelingInfo; // WILL NEED TO BE SET UP FOR EACH WEAPON
 };
 
