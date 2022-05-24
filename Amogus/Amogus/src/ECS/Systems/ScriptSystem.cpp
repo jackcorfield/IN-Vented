e30f3e0193a8ad76c619dc032a2957a4169a933b
Hallett-K <h013486i@student.staffs.ac.uk>
2022-05-24 14:49:21 +0100

    Hotfix
    - Fixed a bug where the main scene wouldn't load due to the entities trying to initialise in the main menu
    - OnAttach and OnUnattach are now called when a scene is loaded or unloaded

--- Amogus/Amogus/src/ECS/Systems/ScriptSystem.cpp
+++ Amogus/Amogus/src/ECS/Systems/ScriptSystem.cpp
@@ -4,6 +4,23 @@
 
 extern Application* g_app;
 
+void ScriptSystem::OnAttach()
+{
+	Scene* currentScene = g_app->m_sceneManager->GetActiveScene();
+	if (!currentScene)
+		return;
+
+	std::vector<ScriptComponent*> scriptComponents = currentScene->m_entityManager->GetAllComponentsOfType<ScriptComponent>();
+
+	for (ScriptComponent* s : scriptComponents)
+	{
+		if (s->GetAttachedScript() != nullptr)
+		{
+			s->GetAttachedScript()->OnAttach();
+		}
+	}
+}
+
 void ScriptSystem::OnUpdate(float dt)
 {
 	Scene* currentScene = g_app->m_sceneManager->GetActiveScene();
@@ -34,6 +51,23 @@ void ScriptSystem::OnRender(float dt)
 		if (s->GetAttachedScript() != NULL)
 		{
 			s->GetAttachedScript()->OnRender(dt);
+		}
+	}
+}
+
+void ScriptSystem::OnUnattach()
+{
+	Scene* currentScene = g_app->m_sceneManager->GetActiveScene();
+	if (!currentScene)
+		return;
+
+	std::vector<ScriptComponent*> scriptComponents = currentScene->m_entityManager->GetAllComponentsOfType<ScriptComponent>();
+
+	for (ScriptComponent* s : scriptComponents)
+	{
+		if (s->GetAttachedScript() != nullptr)
+		{
+			s->GetAttachedScript()->OnUnattach();
 		}
 	}
 }
