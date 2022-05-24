c059d423dfb98f569ea239431f58367fe7f4147b
Hallett-K <43853509+Hallett-K@users.noreply.github.com>
2022-05-24 10:13:03 +0100

    Feature/automation (#103)
    * Freetype copy
    
    - Copies freetype dll to target folder
    - Updated gitignore to ignore DLLs and DebugLog in YokaiFortune
    
    * Automation
    
    - Can export data from VS to Runtime or Build
    - Can launch build from runtime
    
    * Build type detection
    
    - Now uses preprocessor to determine where to copy data

--- Amogus/AmogusEditor/src/ImGuiLayer.cpp
+++ Amogus/AmogusEditor/src/ImGuiLayer.cpp
@@ -208,7 +208,7 @@ void ImGuiLayer::DrawMenuBar()
 			}
 
 			ImGui::Separator();
-			if (ImGui::MenuItem(ICON_FA_FILE_EXPORT"	Export and Run"))
+			if (ImGui::MenuItem(ICON_FA_FILE_EXPORT"	Export and Run from Build"))
 			{
 				if (m_sceneLoaded)
 					SaveScene();
@@ -217,6 +217,63 @@ void ImGuiLayer::DrawMenuBar()
 					SaveGame();
 					std::string command = "YokaiFortune.exe -game " + game.Name;
 					system(command.c_str());
+				}
+				else
+					m_popup = std::make_unique<ErrorDialogGui>("No game loaded");
+			}
+
+			if (ImGui::MenuItem(ICON_FA_FILE_EXPORT"	Export and Run from VS"))
+			{
+				if (m_sceneLoaded)
+					SaveScene();
+				if (m_gameLoaded)
+				{
+					SaveGame();
+					std::string buildType;
+#ifdef _DEBUG
+					buildType = "Debug";
+#else
+					buildType = "Release";
+#endif
+					std::string command = ".\\..\\x64\\" + buildType + "\\YokaiFortune.exe - game " + game.Name;
+					system(command.c_str());
+				}
+				else
+					m_popup = std::make_unique<ErrorDialogGui>("No game loaded");
+			}
+
+			if (ImGui::MenuItem(ICON_FA_FILE_EXPORT"	Export Game Data from VS to Runtime"))
+			{
+				if (m_sceneLoaded)
+					SaveScene();
+				if (m_gameLoaded)
+				{
+					SaveGame();
+					system("xcopy .\\Assets .\\..\\YokaiFortune\\Assets /I /E");
+					system("xcopy .\\Data .\\..\\YokaiFortune\\Data /I /E");
+					system("xcopy .\\Fonts .\\..\\YokaiFortune\\Fonts /I /E");
+				}
+				else
+					m_popup = std::make_unique<ErrorDialogGui>("No game loaded");
+			}
+
+			if (ImGui::MenuItem(ICON_FA_FILE_EXPORT"	Export Game Data from VS to Build"))
+			{
+				if (m_sceneLoaded)
+					SaveScene();
+				if (m_gameLoaded)
+				{
+					SaveGame();
+
+					std::string buildType;
+#ifdef _DEBUG
+					buildType = "Debug";
+#else
+					buildType = "Release";
+#endif
+					system(std::string("xcopy .\\Assets .\\..\\x64\\" + buildType + "\\Assets / I / E").c_str());
+					system(std::string("xcopy .\\Data .\\..\\x64\\" + buildType + "\\Data / I / E").c_str());
+					system(std::string("xcopy .\\Fonts .\\..\\x64\\" + buildType + "\\Fonts / I / E").c_str());
 				}
 				else
 					m_popup = std::make_unique<ErrorDialogGui>("No game loaded");
