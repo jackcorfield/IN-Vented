d1976c5c989bd966b0a7d19743e397389957a925
AlexParcell <alexparcell@gmail.com>
2022-05-24 15:21:52 +0100

    Final timer commit I swear please don't hurt me

--- Amogus/YokaiFortune/src/TimerScript.cpp
+++ Amogus/YokaiFortune/src/TimerScript.cpp
@@ -19,15 +19,17 @@ void TimerScript::OnUpdate(float dt)
 	m_seconds += dt;
 	UI_Text* text = (UI_Text*)GetComponent<UI_WidgetComponent>()->m_elements[0];
 
-	m_minutes = (int)m_seconds / 60;
-	std::string displayMinutes = std::to_string((int)m_minutes);
-	if (m_minutes < 10)
+	int minutes = ((int)m_seconds) / 60;
+	int secondsToDisplay = ((int)m_seconds) % 60;
+
+	std::string displayMinutes = std::to_string(minutes);
+	if (minutes < 10)
 	{
 		displayMinutes = std::string("0") + displayMinutes;
 	}
 
-	std::string displaySeconds = std::to_string(((int)m_seconds)%60);
-	if (m_seconds < 10)
+	std::string displaySeconds = std::to_string(secondsToDisplay);
+	if (secondsToDisplay < 10)
 	{
 		displaySeconds = std::string("0") + displaySeconds;
 	}
