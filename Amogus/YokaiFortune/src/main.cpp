9eaffbabba3f99db0c21a49040995d7e33dce2dc
Jack Corfield <jackcorfield1999@gmail.com>
2022-05-25 11:39:38 +0100

    Disable Equipment for testing

--- Amogus/YokaiFortune/src/main.cpp
+++ Amogus/YokaiFortune/src/main.cpp
@@ -175,31 +175,31 @@ public:
 		scriptC = entityManager->GetComponent<ScriptComponent>(oImplant);
 		if (scriptC)
 		{
-			scriptC->AttachScript<OpticalImplant>(player, oImplant);
+			//scriptC->AttachScript<OpticalImplant>(player, oImplant);
 		}
 
 		scriptC = entityManager->GetComponent<ScriptComponent>(passport);
 		if (scriptC)
 		{
-			scriptC->AttachScript<Passport>(player, passport);
+			//scriptC->AttachScript<Passport>(player, passport);
 		}
 
 		scriptC = entityManager->GetComponent<ScriptComponent>(ragnite);
 		if (scriptC)
 		{
-			scriptC->AttachScript<Ragnite>(player, ragnite);
+			//scriptC->AttachScript<Ragnite>(player, ragnite);
 		}
 
 		scriptC = entityManager->GetComponent<ScriptComponent>(pGlove);
 		if (scriptC)
 		{
-			scriptC->AttachScript<PowerGlove>(player, pGlove);
+			//scriptC->AttachScript<PowerGlove>(player, pGlove);
 		}
 
 		scriptC = entityManager->GetComponent<ScriptComponent>(pGem);
 		if (scriptC)
 		{
-			scriptC->AttachScript<PowerGem>(player, pGem);
+			//scriptC->AttachScript<PowerGem>(player, pGem);
 		}
 
 #pragma endregion
